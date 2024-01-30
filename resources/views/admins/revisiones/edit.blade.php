3b8a38254368f47d5b14c9382cf15e3fad1d8c54
jhon <aldbells@gmail.com>
2024-01-30 02:20:18 -0500

    CRUD PORTAFOLIOS:Revisados (Push para juntar Jhon:Kala)

--- resources/views/admins/revisiones/edit.blade.php
+++ resources/views/admins/revisiones/edit.blade.php
@@ -1,59 +1,76 @@
 @extends('adminlte::page')
 
-@section('title', 'Editar Semestre')
+@section('title', 'Editar Revision')
 
 @section('content_header')
-  <h1 class="text-center"><b>Editar Semestre</b></h1>
+  <h1 class="text-center"><b>Editar Revision</b></h1>
 @stop
 
 @section('content')
 
 <div class="card">
     <div class="card-body">
-        <form action="{{ route('semestres.update', $semestre) }}" method="POST">
+        <form action="{{ route('revisiones.update', $revisione) }}" method="POST">
             @csrf
             @method('PUT')
 
-           <!-- Nombre -->
-           <div class="form-group row">
-            <label class="col-sm-1 col-form-label" for="nombre">Nombre:</label>
-            <div class="col-sm-10">
-                <input type="text" class="form-control" id="nombre" name="nombre" value="{{ old('nombre',$semestre->nombre) }}">
+           <!-- Docente -->
+            <div class="form-group row">
+                <label class="col-sm-1 col-form-label" for="nombre">Docente:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="nombre" name="nombre" value="{{ $revisione->portafolio->cargaAcademica->docente->user->name }}" disabled>
+                </div>
+                <x-input-error :messages="$errors->get('nombre')" class="mt-2" />
             </div>
-            <x-input-error :messages="$errors->get('nombre')" class="mt-2" />
-        </div>
 
-        <!-- Fecha Inicio -->
-        <div class="form-group row">
-            <label class="col-sm-1 col-form-label" for="fecha_inicio">Fecha de Inicio:</label>
-            <div class="col-sm-10">
-                <input type="date" class="form-control" id="fecha_inicio" name="fecha_inicio" value="{{ old('fecha_inicio',$semestre->fecha_inicio) }}">
+            <!-- Revisor -->
+            <div class="form-group row">
+                <label class="col-sm-1 col-form-label" for="nombre">Revisor:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="nombre" name="nombre" value="{{ $revisione->revisor->user->name }}" disabled>
+                </div>
+                <x-input-error :messages="$errors->get('nombre')" class="mt-2" />
             </div>
-            <x-input-error :messages="$errors->get('fecha_inicio')" class="mt-2" />
-        </div>            
 
-        <!-- Fecha Fin -->
-        <div class="form-group row">
-            <label class="col-sm-1 col-form-label" for="fecha_fin">Fecha de Fin:</label>
-            <div class="col-sm-10">
-                <input type="date" class="form-control" id="fecha_fin" name="fecha_fin" value="{{ old('fecha_fin',$semestre->fecha_fin) }}">
+            <!-- Asignatura -->
+            <div class="form-group row">
+                <label class="col-sm-1 col-form-label" for="nombre">Asignatura:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="nombre" name="nombre" value="{{ $revisione->portafolio->cargaAcademica->asignatura->nombre }}" disabled>
+                </div>
+                <x-input-error :messages="$errors->get('nombre')" class="mt-2" />
             </div>
-            <x-input-error :messages="$errors->get('fecha_fin')" class="mt-2" />
-        </div>            
 
-        <!-- Estado -->
-        <div class="form-group row">
-            <label class="col-sm-1 col-form-label" for="estado">Estado:</label>
-            <div class="col-sm-10">
-                <input type="text" class="form-control" id="estado" name="estado" value="{{ old('estado',$semestre->estado) }}">
+            <!-- Numero de revision -->
+            <div class="form-group row">
+                <label class="col-sm-1 col-form-label" for="nombre">Numero de revision:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="nombre" name="nombre" value="{{ $revisione->numero_revision }}" disabled>
+                </div>
+                <x-input-error :messages="$errors->get('nombre')" class="mt-2" />
             </div>
-            <x-input-error :messages="$errors->get('estado')" class="mt-2" />
-        </div>
 
+            <!-- Fecha de revision -->
+            <div class="form-group row">
+                <label class="col-sm-1 col-form-label" for="nombre">Fecha de revision:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="nombre" name="nombre" value="{{ $revisione->fecha_revision }}" disabled>
+                </div>
+                <x-input-error :messages="$errors->get('nombre')" class="mt-2" />
+            </div>
+
+            <!-- observaciones -->
+            <div class="form-group row">
+                <label class="col-sm-1 col-form-label" for="nombre">Observacion:</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="observaciones" name="observaciones" value="{{ $revisione->observaciones }}">
+                </div>
+                <x-input-error :messages="$errors->get('nombre')" class="mt-2" />
+            </div>          
 
             <div class="text-center" >                
-                <input type="submit" value="Editar Semestre" class="btn btn-primary">             
-                <a href="{{ route('semestres.index') }}" class="btn btn-secondary">Cancelar</a>
+                <input type="submit" value="Editar Revision" class="btn btn-primary">             
+                <a href="{{ route('revisiones.index') }}" class="btn btn-secondary">Cancelar</a>
             </div>
         </form>
     </div>
